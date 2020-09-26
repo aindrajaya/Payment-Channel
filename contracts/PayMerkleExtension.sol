e54cb58255c4e0a204f23c71885b65986b3440cd
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-09-26 22:59:04 +0700

    upgrade for newest/stabel solidity compiler

--- contracts/PayMerkleExtension.sol
+++ contracts/PayMerkleExtension.sol
@@ -1,41 +1,41 @@
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract PayMerkleExtended {
-    address public channelSender;
-    address public channelRecipient;
+    address payable public channelSender;
+    address payable public channelRecipient;
     uint public startDate;
     uint public channelTimeout;
-    bytes32 public root;
+    bytes32 public roots;
 
-    function PayMerkleExtended(address to, uint _timeout, bytes32 _root) public payable {
+    constructor (address payable to, uint _timeout, bytes32 _root) public payable {
         require(msg.value>0);
         channelRecipient = to;
         channelSender = msg.sender;
         startDate = now;
         channelTimeout = _timeout;
-        root = _root;
+        roots = _root;
     }
     function AddBalance(bytes32 _newRoot) public payable {
-      if (root < _newRoot)
-          root = keccak256(root, _newRoot);
-      else
-          root = keccak256(_newRoot, root);
+        if (roots < _newRoot)
+            roots = keccak256(abi.encodePacked(roots, _newRoot));
+        else
+            roots = keccak256(abi.encodePacked(_newRoot, roots));
     }
-  function CloseChannel(uint256 _amount, bytes32[] proof) public {
+    function CloseChannel(uint256 _amount, bytes32[] memory proof) public  {
         require(msg.sender==channelRecipient);
-        bytes32 computedHash = keccak256(_amount);
-        require(verifyMerkle(root, computedHash, proof));
+        bytes32 computedHash = keccak256(abi.encodePacked(_amount));
+        require(verifyMerkle(roots, computedHash, proof));
         channelRecipient.transfer(_amount);
         selfdestruct(channelSender);
     }
-    function verifyMerkle (bytes32 root, bytes32 leaf, bytes32[] proof) public pure returns (bool) {
-      bytes32 computedHash = leaf;
-      for (uint256 i = 0; i < proof.length; i++) {
-          if (computedHash < proof[i])
-            computedHash = keccak256(computedHash, proof[i]);
-          else
-            computedHash = keccak256(proof[i], computedHash);
-          }
+    function verifyMerkle (bytes32 root, bytes32 leaf, bytes32[] memory proof) public pure returns (bool) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            if (computedHash < proof[i])
+                computedHash = keccak256(abi.encodePacked(computedHash, proof[i]));
+            else
+                computedHash = keccak256(abi.encodePacked(proof[i], computedHash));
+        }
         return computedHash==root;
     }
     function ChannelTimeout() public {
